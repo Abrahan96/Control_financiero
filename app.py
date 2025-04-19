f50569ff8bcba66bb68538aabbd5abf01750303a
Abrahan96 <perezccatamayoabrahan@gmail.com>
2025-04-18 22:02:59 -0500

    Creando login

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
+
+st.set_page_config(page_title="Control Financiero", layout="centered")
+
 from modules.formulario import formulario
 from modules.visualizacion import visualizacion
 from modules.historial import historial
 from modules.backup import backup
 from auth.login import login
 
-
-st.set_page_config(page_title="Control Financiero", layout="centered")
-
 if login():
-
     st.sidebar.title("📊 Navegación")
 
     opcion = st.sidebar.radio("Ir a:", ["Formulario", "Visualización", "Historial", "Backup"])
