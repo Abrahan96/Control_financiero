be38d21ac2c146d84eb57e78c104f60dcff1772b
Abrahan96 <perezccatamayoabrahan@gmail.com>
2025-04-18 19:01:39 -0500

    Creando formulario de registros

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from modules.formulario import formulario
 
 st.set_page_config(page_title="Control Financiero", layout="centered")
+
 st.sidebar.title("📊 Navegación")
 
 opcion = st.sidebar.radio("Ir a:", ["Formulario"])
